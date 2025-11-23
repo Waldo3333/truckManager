2b6874db490c2686fb7ae8103f94569ddf45d7ca
charlie gardere <charlie.gardere@gmail.com>
2025-11-23 18:58:28 +0100

    Sortable modifié

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,5 @@
 // app/javascript/controllers/index.js
-import { application } from "./application.js";
+import { application } from "controllers/application";
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
