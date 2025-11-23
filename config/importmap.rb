ffc7144055a2a8a9957a47e21793b1ab9bb6f66c
charlie gardere <charlie.gardere@gmail.com>
2025-11-23 18:18:47 +0100

    Sortable modifié

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,15 @@
-# Pin npm packages by running ./bin/importmap
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+# Pin explicite de chaque controller
+pin "controllers/application", to: "controllers/application.js", preload: true
+pin "controllers/hello_controller", to: "controllers/hello_controller.js", preload: true
+pin "controllers/index", to: "controllers/index.js", preload: true
+pin "controllers/info_card_controller", to: "controllers/info_card_controller.js", preload: true
+pin "controllers/modal_controller", to: "controllers/modal_controller.js", preload: true
+pin "controllers/employee_schedule_controller", to: "controllers/employee_schedule_controller.js", preload: true
+
+# Planning
 pin "planning", preload: true
