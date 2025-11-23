a97a834bffba90ebe8ad9ba986598a6b5ca61b9f
charlie gardere <charlie.gardere@gmail.com>
2025-11-23 18:00:18 +0100

    Sortable modifié

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,6 @@
-# config/importmap.rb
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-
-# Ajouter ces lignes :
-pin "planning", to: "planning.js"
+pin_all_from "app/javascript/lib", under: "lib"
