65f78c585c531549aab97da557ad17f662098d4e
charlie gardere <charlie.gardere@gmail.com>
2025-11-23 18:51:02 +0100

    Sortable modifié

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,19 +1,12 @@
 // app/javascript/controllers/index.js
+
+// Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "./application";
-
-import HelloController from "./hello_controller";
-application.register("hello", HelloController);
-
-import InfoCardController from "./info_card_controller";
-application.register("info-card", InfoCardController);
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+eagerLoadControllersFrom("controllers", application);
 
 import ModalController from "./modal_controller";
 application.register("modal", ModalController);
 
 import EmployeeScheduleController from "./employee_schedule_controller";
 application.register("employee-schedule", EmployeeScheduleController);
-
-import PlanningController from "./planning_controller";
-application.register("planning", PlanningController);
-
-export { application };
